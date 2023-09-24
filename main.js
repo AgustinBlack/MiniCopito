37ed828acf6228c354906aec0fbaf473c61cac2c
Agustin Black <agustinblack04@gmail.com>
2023-09-24 15:13:26 -0300

    seventh commit

--- main.js
+++ main.js
@@ -38,6 +38,14 @@ const arrayDeCatalogo = [
         estilo: "Conjunto",
         talle: "1-2-3-4",
         precio: 2000
+    },
+    {
+        id: 6,
+        nombre: "Vestido floreado con puntilla",
+        imagen: "./images/vestido.jpeg",
+        estilo: "Vestido",
+        talle: "1-2-3-4",
+        precio: 2800
     }
 ]
 
@@ -51,7 +59,7 @@ function mostrarProductos(arrayProductos) {
             <div class="divCard">
                 <img class="divImg" src="${producto.imagen}" alt="">
                 <div class="divInfo">
-                    <h3 class="divInfoNombre">${producto.nombre}</h3>
+                    <h3 class="divInfoNombre" onmouseover="startScroll(this)" onmouseout="stopScroll(this)">${producto.nombre}</h3>
                     <h6 class="divInfoEstilo">${producto.estilo}</h6>
                     <p class="divInfoTalle">Talle: ${producto.talle}</p>
                     <p class="divInfoPrecio">$${producto.precio}</p>
@@ -64,3 +72,19 @@ function mostrarProductos(arrayProductos) {
 }
 
 mostrarProductos(arrayDeCatalogo)
+
+function startScroll(card) {
+    let productName = card.querySelector('.divInfoNombre');
+    let scrollWidth = productName.scrollWidth;
+    let clientWidth = productName.clientWidth;
+
+    if (scrollWidth > clientWidth) {
+        var animationDuration = (scrollWidth - clientWidth) * 20;
+        productName.style.animation = `scroll-left ${animationDuration}ms linear infinite`;
+    }
+}
+  
+function stopScroll(card) {
+    let productName = card.querySelector('.divInfoNombre');
+    productName.style.animation = 'none';
+}
